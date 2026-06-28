e337c7b76324840b6cf80ddef1c61c41ffffae8b
Skye Nguyen <its.ssskyler@gmail.com>
2026-06-28 16:28:12 +0700

    Initial Commit

---
+++ RotAway/RotAwayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotAwayApp.swift
+//  RotAway
+//
+//  Created by Skye Nguyen on 28/6/26.
+//
+
+import SwiftUI
+
+@main
+struct RotAwayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
